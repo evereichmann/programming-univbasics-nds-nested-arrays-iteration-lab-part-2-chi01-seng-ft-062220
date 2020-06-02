0e5791a7f142149cc9bb013fd66395c9eaecaf6c
Eve Reichmann <ereichmann4@aol.com>
2020-06-02 23:24:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,14 +3,14 @@ outer_results = []
 row_index = 0
 while row_index < src.count do 
   element_index = 0
-  lowest_value = 65
+  min_daily_temp = 65
 while element_index < src[row_index].count do
-  if src[row_index][element_index] < lowest_value
-    lowest_value = src[row_index][element_index]
+  if src[row_index][element_index] < min_daily_temp
+    min_daily_temp = src[row_index][element_index]
 end
 element_index += 1
 end
-outer_results << lowest_value
+outer_results << min_daily_temp
 row_index += 1
 end
 outer_results
