d81ed952d700fc035d853bb990ed9a81f7a6640f
Eve Reichmann <ereichmann4@aol.com>
2020-06-02 23:18:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,13 +3,15 @@ outer_results = []
 row_index = 0
 while row_index < src.count do 
   element_index = 0
-
+  inner_result = [] 
+  lowest_value = 100
 while element_index < src[row_index].count do
-  if src[row_index][element_index] == src.
-    outer_results << src[row_index][element_index]
+  if src[row_index][element_index] < lowest_value
+    lowest_value = src[row_index][element_index]
 end
 element_index += 1
 end
+outer_results << lowest_value
 row_index += 1
 end
 outer_results
